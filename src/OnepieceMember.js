c1e12f194eca91556346c943145b7d53b0d200d5
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-23 12:23:01 +0900

    커밋테스트 및 수업용

--- src/OnepieceMember.js
+++ src/OnepieceMember.js
@@ -1,11 +1,15 @@
 export default function OnePieceMember({ emoji, role, desc, name }) {
-    return (
-        <div className="crew-card">
-            <div className="img-placeholder">{emoji}</div>
-            <div className="info">
-                <h2>{name}</h2>
-                <p><strong>{role}</strong> – {desc}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="crew-card">
+      <div className="img-placeholder">{emoji}</div>
+      <div className="info">
+        <h2>
+          {name} {role === "선장" && <small>리더</small>}
+        </h2>
+        <p>
+          <strong>{role}</strong> – {desc}
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-import Demo from './Demo';
-import Root from './Root';
-import Nav from './Nav';
-import OnePiece from './OnePiece';
+import OnePiece from "./OnePiece";
+import Exercise from "./Exercise";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-   <OnePiece />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Exercise />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

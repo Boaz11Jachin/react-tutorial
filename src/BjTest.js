476222101df67746e9f9fda025ae2aaa68a23a08
Boaz11Jachin <sheepo84k@gmail.com>
2025-04-23 10:39:10 +0900

    chore : 리액트 1일차 수업완료

---
+++ src/BjTest.js
@@ -0,0 +1,13 @@
+
+
+const BjTest = function () {
+  return (<div>
+    <h1>리액트</h1>
+    <p>
+      짓쥬료쿠!
+    </p>
+  </div>);
+}
+
+
+export default BjTest;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Demo from './Demo';
+import Root from './Root';
+import Nav from './Nav';
+import OnePiece from './OnePiece';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <OnePiece />
 );
 
 // If you want to start measuring performance in your app, pass a function
